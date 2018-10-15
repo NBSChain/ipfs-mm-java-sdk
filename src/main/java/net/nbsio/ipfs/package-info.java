e578b0949d45078314e72c2cd9de85b1eb958818
lanbery <lanbery@gmail.com>
2018-10-15 16:03:08 +0800

    创建IPFS SDK 项目

---
+++ src/main/java/net/nbsio/ipfs/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright © 2015-2020 NBSChain Holdings Limited.
+ * All rights reserved.
+ *
+ * @project ipfs-mm
+ * IPFS SDK EXT.
+ * Author   : lanbery
+ * Created  : 2018/10/15
+ */
+package net.nbsio.ipfs;
